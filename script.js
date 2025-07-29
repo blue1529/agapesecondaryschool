70093a02ac3a423ee1da48f2e1b0a35637a0395b
Newton Madalitso Kalulu <newtonkalulu00@gmail.com>
2025-07-29 14:11:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector('.scroll-container');
+
+    // Remove or comment out the autoscroll code below
+    /*
+    let scrollAmount = 0;
+    setInterval(() => {
+        if (scrollAmount < container.scrollWidth - container.clientWidth) {
+            scrollAmount += 210; // width + gap
+        } else {
+            scrollAmount = 0;
+        }
+        container.scrollTo({ left: scrollAmount, behavior: 'smooth' });
+    }, 4000); // change slide every 3s
+    */
+
+function toggleGallery(tile) {
+  tile.classList.toggle('active');
+}
+//opening the hamburger
+    const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+    
+    //changing hamburger shape
+    hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+    hamburger.textContent = navLinks.classList.contains('show') ? '✖' : '☰';
+});
+document.addEventListener('click', function(event) {
+    const isClickInsideNav = navLinks.contains(event.target) || hamburger.contains(event.target);
+
+    if (!isClickInsideNav) {
+        navLinks.classList.remove('show');
+        hamburger.textContent = '☰'; // Reset to hamburger icon
+    }
+});
+document.getElementById('seeMoreBtn').addEventListener('click', function() {
+    const text = document.getElementById('aboutUsText');
+    text.classList.toggle('expanded');
+    this.textContent = text.classList.contains('expanded') ? 'See Less' : 'See More';
+});
